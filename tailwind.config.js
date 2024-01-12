e2217b28f59a9fa068acb3f1b195ecf193006ac5
Ganesh <ghattiganesh8@gmail.com>
2024-01-12 15:57:05 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  darkMode: "media",
+  theme: {
+    extend: {
+      colors: {
+        white: "#FFF",
+        black: "#000",
+        "blue-2": "#ADD7F6",
+        "blue-1": "#87BFFF",
+        "blue-0": "#3F8EFC",
+        "blue+1": "#2667FF",
+        "blue+2": "#3B28CC",
+        bluepurple: "#4633D7",
+        navyblue: "#1F20AE",
+        blackbg: "#14181B",
+        red: "#C33332",
+        orange: "#D9822B",
+        mainbg: "#1D2428",
+        gray: "#95A1AC",
+        blue: "#3a32a7",
+      },
+    },
+
+    screens: {
+      sm: { max: "639px" },
+      md: { max: "866px" },
+      lg: { min: "867px", max: "1100px" },
+      xl: { min: "1100px", max: "1279px" },
+      "2xl": { min: "1280px", max: "1335px" },
+    },
+  },
+};
